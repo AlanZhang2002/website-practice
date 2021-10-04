01757b1addd68297c59872d697229ceed9337377
AlanZhang2002 <alanmz2002@gmail.com>
2021-10-04 17:23:09 -0400

    started etch-a-sketch project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# website-practice
+# Website Practice
+
+A repository consisting of different small self-guided website projects. 
+Currently self-learning basic web development.
--- etch-a-sketch-project/script.js
+++ etch-a-sketch-project/script.js
@@ -1,15 +1,25 @@
+const DEFAULT_COLOR = '#aaaaaa';
+const DEFAULT_MODE = 'color';
+const DEFAULT_SIZE = 16;
 
+let currentColor = DEFAULT_COLOR;
+let currentMode = DEFAULT_MODE;
+let currentSize = DEFAULT_SIZE;
+
+const grid = document.getElementById('grid');
 
 function setGrid(size) {
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
-            let div = document.createElement("div");
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            const div = document.createElement("div");
             div.addEventListener('mouseover', updateColor)
-
+            grid.appendChild(div);
         }
     }
 }
 
-function updateColor() {
-
+function updateColor(e) {
+    if (currentMode === 'color') {
+        e.target.style.backgroundColor = currentColor;
+    }
 }
