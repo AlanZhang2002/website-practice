3791f15e39560ab9e09da3e251fcc4cb34584064
AlanZhang2002 <alanmz2002@gmail.com>
2022-01-16 12:05:20 -0500

    added DOM manipulation to cheatsheet, updated rps project with simple html ui

--- random-files/cheatsheet.js
+++ random-files/cheatsheet.js
@@ -17,12 +17,12 @@ console.log("Hello World!!");
 
 // Arithmetic - consists of basic math operations, similar to other languages. Follows PEMDAS.
 // Shorthand works similarly to Java as well (+=, -=, *=, /=, ++, --)
-alert(3+3);  // Addition
-alert(2-1);  // Subtraction
-alert(5*4);  // Multiplication
-alert(2**3); // Exponentiation
-alert(8/2);  // Division
-alert(10%3); // Modulus (remainder)
+alert(3 + 3);  // Addition
+alert(2 - 1);  // Subtraction
+alert(5 * 4);  // Multiplication
+alert(2 ** 3); // Exponentiation
+alert(8 / 2);  // Division
+alert(10 % 3); // Modulus (remainder)
 
 // Data Types - Strings, Numbers, Booleans, BigInt (larger than 2^53 in magnitude), Object, null, undefined, Symbol
 let exampleNumber = 10;
@@ -52,17 +52,34 @@ if (true) {
 function addNumbers(num1, num2) {
     return num1 + num2;
 }
+
 function showMessage(from, text = "no text given") {
     return from + ": " + text;
     alert(text ?? "no text given"); // If there was no default value specified, this works similarly
 }
+
 function lThings(thing) {
     return thing.startsWith('L');
 }
 
+// Arrays
+// counts as Objects, but each element is accessed by the index number, not a name
+const exampleArray = ["Joe", "Bill"];
+exampleArray.push("Bob"); // adds value to end of array
+alert(exampleArray[0]);
+exampleArray[1] = "Billy";
+alert(exampleArray.length);
+exampleArray[3] = "Jeff"; // can add element to an undefined array index
+alert(exampleArray.pop);
+alert(exampleArray.shift());
+exampleArray.unshift("Joe");
+alert(exampleArray.toString());
+const concatArray = exampleArray.concat(["Jeremy, Bryan"]);
+alert(concatArray.slice(1, 3).toString()); // inclusive starting, exclusive ending. Can have 1 parameter.
+
 // Loops
 // While loop
-while (true) {
+while (true || false) {
     // Code here
 }
 // Do while loop
@@ -84,9 +101,46 @@ things2 = things.map((thing) => thing.toUpperCase()); // makes everything upperc
 // filter() - returns new array filtered by a boolean function from existing array
 let things3 = things.filter(lThings);
 things3 = things.filter((thing) => thing.startsWith('L')); // same thing, in one line
-// Label - can identify a loop, to break out of/continue from
+// Label - can identify a loop, to break out of/continue from (good for nested loops)
 outer: for (let i = 0; i < 3; i++) {
     if (true) {
         break outer;
     }
 }
+
+// DOM manipulation
+// Note: script is run when encountered in HTML, so either put js at bottom of HTML or add 'defer' tag to have js loaded after HTML is parsed
+// Example: <script src="js-file.js" defer></script>
+const container = document.querySelector('#container'); // gets the node with id='container'
+const manyContainers = document.querySelectorAll('div'); // gets all div, as a nodelist (similar to array, with a few missing functions)
+console.dir(container.firstElementChild); // gets first element within container
+const div = document.createElement('div'); // creates new element in memory
+container.firstElementChild.appendChild(div); // adds div as a child of the first element of container
+container.firstElementChild.removeChild(div); // removes div from first element of container
+container.firstElementChild.insertBefore(div, container.firstElementChild.firstElementChild) // inserts div before the first child element of the first element of container
+// Style
+div.style.color = 'blue'; // sets div's attributes (see http://domenlightenment.com/#6.2)
+div.style.cssText = 'color: blue; background: white'; // multiple style rules
+div.setAttribute('style', 'color: blue; background: white'); // same as above
+// ID
+div.setAttribute('id', 'newId'); // either updates current id to newId or adds new id
+div.getAttribute('id');
+div.removeAttribute('id');
+// Class
+div.classList.add('new'); // adds a new class called "new"
+div.classList.remove('new'); // removes "new" class
+div.classList.toggle('new'); // if "new" exists, remove. if not, add.
+// Text
+div.textContent = 'Hello World';
+// HTML
+div.innerHTML = '<span>Hello World</span>'; // potentially dangerous to injection
+// Events (list of events: https://www.w3schools.com/jsref/dom_obj_event.asp)
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        alert(button.id);
+    });
+});
+buttons[1].addEventListener('click', function (e) {
+    console.log(e.target);
+});
--- rps-project/script.js
+++ rps-project/script.js
@@ -1,34 +1,54 @@
-function game() {
-    let wins = 0, losses = 0;
-    for (let i = 0; i < 5; i++) {
-        const input = prompt("Pick rock, paper, or scissors: ");
-        console.log(playRound(input, computerPlay()));
-        if (playRound(input, computerPlay()).includes("won", 4)) {
-            wins++;
-        } else if (playRound(input, computerPlay()).includes("lost", 4)) {
-            losses++;
-        }
-    }
+let playerScore = 0, computerScore = 0;
+let finished = false;
 
-    finishGame(wins, losses);
-}
+const buttons = document.querySelectorAll('button');
+const result = document.querySelector('.result');
+const score = document.querySelector('.score');
+const resultFinal = document.querySelector('.result-final');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        result.textContent = playRound(button.textContent, computerPlay());
+        score.textContent = playerScore + ' | ' + computerScore;
+        if ((playerScore === 5 || computerScore === 5) && !finished) {
+            finishGame(playerScore, computerScore);
+        }
+    });
+})
+
+// -=-=-=- Old code -=-=-=-
+// function game() {
+//     let wins = 0, losses = 0;
+//     for (let i = 0; i < 5; i++) {
+//         const input = prompt("Pick rock, paper, or scissors: ");
+//         console.log(playRound(input, computerPlay()));
+//         if (playRound(input, computerPlay()).includes("won", 4)) {
+//             wins++;
+//         } else if (playRound(input, computerPlay()).includes("lost", 4)) {
+//             losses++;
+//         }
+//     }
+//
+//     finishGame(wins, losses);
+// }
 
 function finishGame(wins, losses) {
     if (wins > losses) {
-        console.log("You beat the computer!")
-    } else if (wins < losses) {
-        console.log("The computer won!")
+        resultFinal.textContent = 'You beat the computer!';
     } else {
-        console.log("It was a tie!")
+        resultFinal.textContent = 'The computer won!';
     }
+    finished = true;
 }
 
 function playRound(playerSelection, computerSelection) {
     if (checkWin(playerSelection, computerSelection)) { // Win
+        playerScore++;
         return "You won! Computer chose " + computerSelection;
     } else if (checkTie(playerSelection, computerSelection)) { // Tie
         return "It was a tie! You both chose " + computerSelection;
     } else { // Lose
+        computerScore++;
         return "You lost! Computer chose " + computerSelection;
     }
 }
@@ -53,7 +73,3 @@ function computerPlay() {
         return "Scissors";
     }
 }
-
-// const playerSelection = "rock"
-// const computerSelection = computerPlay();
-// console.log(playRound(playerSelection, computerSelection));
